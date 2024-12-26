4e9832253553034203dd4748c1df73829a728462
Anderson T <tseng.andersonn@gmail.com>
2024-12-25 22:37:25 -0800

    refactor to add more pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,22 +3,49 @@ export const metadata = {
   description: "A web application to search and explore Langara College course offerings",
 };
 
-import CourseBrowser from "./CourseBrowser";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="w-full h-full">
-      <header className="p-5 bg-[#F15A22]">
-        <h1 className="font-bold text-xl">Langara Course Offerings Search</h1>
+
+      <header className="p-5 bg-[#ff926a]">
+        <h1 className="font-bold text-xl">Langara College Course Tools</h1>
         <p>Note: this website is a student project and not affiliated with Langara College.</p>
-        <p>Inspired by the <a href="https://coursys.sfu.ca/browse" target="_blank">SFU CourSys</a>. Please report bugs or suggestions at <a className="hover:underline hover:text-blue-800" href="https://forms.gle/CYKP7xsp2an6gNEK9" target="_blank">this form.</a></p>
+        <p>Utilities built by Anderson Tseng and hosted by the Langara Computer Science Club.</p>
+        <p>Please report bugs or suggestions at <a className="hover:underline hover:text-blue-800" href="https://forms.gle/CYKP7xsp2an6gNEK9" target="_blank">this form.</a></p>
         {/* <p>Data last updated ...</p> */}
       </header>
 
-      <div className="md:px-10">
-        <CourseBrowser/>
+      <div className="md:px-10 pt-10 flex flex-row gap-4">
+        
+        {/* <h1>Welcome to the Langara Course Tools Hub</h1>
+        <p>You can find some tools below:</p> */}
+
+
+        <div className="w-min h-min">
+          <Link href="https://planner.langaracs.ca">
+            <div className="w-[400px] h-[400px] bg-slate-300 flex items-center justify-center">
+              Langara Course Planner
+            </div>
+          </Link>
+        </div>
+
+        <div className="w-min h-min">
+          <Link href="/browse">
+            <div className="w-[400px] h-[400px] bg-slate-300 flex items-center justify-center">
+              Langara Course Search
+            </div>
+          </Link>
+        </div>
       </div>
-      
+
+
+
+
+
+
+
     </div>
   );
 }
