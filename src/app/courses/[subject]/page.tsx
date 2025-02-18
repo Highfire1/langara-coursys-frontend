409943664d5509eea47c14fb4bfc164f499781a4
Anderson T <tseng.andersonn@gmail.com>
2025-02-18 14:37:39 -0800

    fix build (next.js moment)

--- src/app/courses/[subject]/page.tsx
+++ src/app/courses/[subject]/page.tsx
@@ -1,8 +1,11 @@
 import { redirect } from 'next/navigation'
 
+type Params = Promise<{ subject: string }>
+
 // TODO: i think this violates html best practices
 // but i don't want to leave it as a 404
 // and /courses has all the information that a /courses/[subject] would have
-export default function CoursePage({ params }: { params: { subject: string } }) {
-    redirect(`/courses?subject=${params.subject.toUpperCase()}`)
+export default async function CoursePage({ params }: { params: Params }) {
+    const { subject } = await params
+    redirect(`/courses?subject=${subject.toUpperCase()}`)
 }
