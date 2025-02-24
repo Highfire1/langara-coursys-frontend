36cc1f2b68beaa23d3cbd06dca1a6bbe853f244b
Anderson T <tseng.andersonn@gmail.com>
2025-02-24 01:04:09 -0800

    fix the build.

--- src/app/courses/[subject]/[coursecode]/page.tsx
+++ src/app/courses/[subject]/[coursecode]/page.tsx
@@ -1,30 +1,31 @@
 import CourseInfo from "./course-info";
 import Header from "@/components/shared/header";
 
-export async function generateMetadata({ params }: { params: { subject: string, coursecode: string } }) {
-    const { subject, coursecode } = params;
+export async function generateMetadata({ params }: { params: Params }) {
+    const { subject, coursecode } = await params;
+
     return {
-        title: `${subject} ${coursecode}`,
-        description: "Information about a given course",
+        title: `${subject.toUpperCase()} ${coursecode}`,
+        description: `Details about ${subject.toUpperCase()} ${coursecode} at Langara College.`,
     };
 }
 
+type Params = Promise<{ subject: string; coursecode: string }>;
+
 export default async function Page({
-    params,
-  }: {
-    params: Promise<{ subject: string, coursecode: string }>;
-  }) {
-    const subject = (await params).subject;
-    const courseCode = (await params).coursecode;
+    params
+}: {
+    params: Params
+}
+) {
+    const { subject, coursecode } = await params;
 
     return (
         <div className="w-full h-full">
             <Header title="Langara Course Information" color="#A7C7E7"></Header>
 
             <div className="md:px-10 py-2">
-                {/* I don't understand why suspense is required here
-                next.js requires it when using useSearchParams, but i don't understand why it can't just handle it... */}
-                <CourseInfo subject={subject} course_code={courseCode} />
+                <CourseInfo subject={subject} course_code={coursecode} />
             </div>
         </div>
     );
