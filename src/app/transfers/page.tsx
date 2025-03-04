6c9305013660ae94e01cf6c60ee38b81478e0bed
Anderson T <tseng.andersonn@gmail.com>
2025-03-03 19:16:15 -0800

    template transfer page

---
+++ src/app/transfers/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const TransfersPage = () => {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen p-4">
+            <h1 className="text-3xl font-bold mb-4">Transfers</h1>
+            <div className="text-xl text-gray-600">
+                🚧 Under Construction 🚧
+            </div>
+        </div>
+    );
+};
+
+export default TransfersPage;
