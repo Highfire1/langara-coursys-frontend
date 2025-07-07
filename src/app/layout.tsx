e61ad60d6dd7c1bdc8535b3e5cb7d19bb704b64c
Anderson T <tseng.andersonn@gmail.com>
2025-07-06 21:43:51 -0700

    plausible -> google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import PlausibleProvider from "next-plausible";
+import { GoogleAnalytics } from '@next/third-parties/google';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,13 +25,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <PlausibleProvider domain="planner.langaracs.ca" customDomain='https://a.langaracs.ca' trackOutboundLinks selfHosted/>
-      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <GoogleAnalytics gaId="G-HV5V79FQ82" />
       </body>
     </html>
   );
