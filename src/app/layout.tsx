98111520d5f1e281d298658d2d2712d8aa0a7362
Anderson T <tseng.andersonn@gmail.com>
2025-07-08 03:54:38 -0700

    more analytics 🤩

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { GoogleAnalytics } from '@next/third-parties/google';
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,7 @@ export default function RootLayout({
         {children}
         <GoogleAnalytics gaId="G-HV5V79FQ82" />
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
